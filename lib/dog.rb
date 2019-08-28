91c3ae68ab3f82f37820da69a4794cfb7a55f360
github username <github email address>
2019-08-28 19:18:32 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+class Dog
+attr_accessor :name
+
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each { |key| puts key.name}
+
+    end
+
+end
+
